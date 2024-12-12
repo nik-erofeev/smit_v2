6161bde306b47f1ee1c42a2b0d7d143916d68e89
Nik <erofeev.nik.it@yandex.ru>
2024-12-12 17:04:18 +0300

    fix Auth_rout response model

--- app/api/auth/router.py
+++ app/api/auth/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Response, status
+from fastapi.responses import ORJSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.auth.auth import authenticate_user, create_access_token
@@ -14,6 +15,9 @@ from app.api.auth.schemas import (
     SUserAuth,
     SUserInfo,
     SUserRegister,
+    UAuthResponse,
+    UserOutResponse,
+    UserRegResponse,
 )
 from app.dao.session_maker import SessionDep
 from app.models import User
@@ -21,11 +25,17 @@ from app.models import User
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-@router.post("/register/")
+@router.post(
+    "/register/",
+    summary="Регистрация пользователя",
+    response_model=UserRegResponse,
+    response_class=ORJSONResponse,
+    status_code=status.HTTP_201_CREATED,
+)
 async def register_user(
     user_data: SUserRegister,
     session: AsyncSession = SessionDep,
-) -> dict:
+):
     user = await UsersDAO.find_one_or_none(
         session=session,
         filters=EmailModel(email=user_data.email),
@@ -35,10 +45,15 @@ async def register_user(
     user_data_dict = user_data.model_dump()
     del user_data_dict["confirm_password"]
     await UsersDAO.add(session=session, values=SUserAddDB(**user_data_dict))
-    return {"message": "Вы успешно зарегистрированы!"}
+    return UserRegResponse()
 
 
-@router.post("/login/")
+@router.post(
+    "/login/",
+    response_model=UAuthResponse,
+    response_class=ORJSONResponse,
+    status_code=status.HTTP_200_OK,
+)
 async def auth_user(
     response: Response,
     user_data: SUserAuth,
@@ -53,23 +68,39 @@ async def auth_user(
         raise IncorrectEmailOrPasswordException
     access_token = create_access_token({"sub": str(check.id)})
     response.set_cookie(key="users_access_token", value=access_token, httponly=False)
-    return {"ok": True, "access_token": access_token, "message": "Авторизация успешна!"}
+    return UAuthResponse(access_token=access_token)
 
 
-@router.post("/logout/")
+@router.post(
+    "/logout/",
+    response_model=UserOutResponse,
+    response_class=ORJSONResponse,
+    status_code=status.HTTP_200_OK,
+)
 async def logout_user(response: Response):
     response.delete_cookie(key="users_access_token")
-    return {"message": "Пользователь успешно вышел из системы"}
+    return UserOutResponse()
 
 
-@router.get("/me/")
-async def get_me(user_data: User = Depends(get_current_user)) -> SUserInfo:
+@router.get(
+    "/me/",
+    response_model=SUserInfo,
+    response_class=ORJSONResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def get_me(user_data: User = Depends(get_current_user)):
     return SUserInfo.model_validate(user_data)
 
 
-@router.get("/all_users/")
+@router.get(
+    "/all_users/",
+    response_model=list[SUserInfo],
+    response_class=ORJSONResponse,
+    status_code=status.HTTP_200_OK,
+)
 async def get_all_users(
     session: AsyncSession = SessionDep,
     user_data: User = Depends(get_current_admin_user),
-) -> list[SUserInfo]:
-    return await UsersDAO.find_all(session=session, filters=None)
+):
+    result = await UsersDAO.find_all(session=session, filters=None)
+    return [SUserInfo.model_validate(user) for user in result]
